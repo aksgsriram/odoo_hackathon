4527c21ea05dafab26eba2c80d7be5c305d2736d
Sriram <aksgsriram@gmail.com>
2026-07-12 10:06:39 +0530

    Create AssetFlow module structure

---
+++ assetflow/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    "name": "AssetFlow",
+    "version": "1.0.0",
+    "summary": "Enterprise Asset & Resource Management System",
+    "description": """
+        AssetFlow helps organizations manage assets,
+        resource booking, maintenance, audits and reports.
+    """,
+    "author": "Your Team Name",
+    "website": "",
+    "category": "Operations",
+    "license": "LGPL-3",
+
+    "depends": [
+        "base",
+        "mail",
+        "hr"
+    ],
+
+    "data": [
+    ],
+
+    "installable": True,
+    "application": True,
+}
---
+++ assetflow/models/__init__.py
@@ -0,0 +1,3 @@
+from . import department
+from . import asset_category
+from . import asset
